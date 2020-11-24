2ff1839a73f890f59d309b19032337b74e0519f7
DESKTOP-BN5JHCD\rhian <rhian.rose@landmark.co.uk>
2020-11-24 11:35:36 +0000

    Added routing in, and set map component as the default page for the website

--- WalkiesWebsite/src/app/app-routing.module.ts
+++ WalkiesWebsite/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapComponent } from './pages/map/map.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MapComponent
+    //Anyone can view this page, so doesn't need to be authorised
+    //Specific components with the html will be specified
+  },
+  {
+    path: 'map',
+    component: MapComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
