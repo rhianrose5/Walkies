939f9f8b656cdd6f32712bf5f7e06a202467558a
DESKTOP-BN5JHCD\rhian <rhian.rose@landmark.co.uk>
2020-11-24 12:48:07 +0000

    Integrated mapbox map

--- WalkiesWebsite/src/app/pages/map/map.component.ts
+++ WalkiesWebsite/src/app/pages/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/rhianrose5/ckhvw0bkg0cwt19o1yz3nz0e7';
+  lat = 50.614814;
+  lng = -3.412887;
+  photos: any;
+  loading = true;
+  commentDate: string;
 
-  ngOnInit() {
+  constructor() {
+
   }
 
+  ngOnInit() {
+    //Replace hard code key with environment variable later
+    Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set("pk.eyJ1IjoicmhpYW5yb3NlNSIsImEiOiJja2h2dzZjc2EweWtiMnNwNTE1M3doMzh2In0.q2Dr4ATPtHLaQ5uRzc1Jcg");
+
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 10,
+      center: [this.lng, this.lat]
+    });
+
+    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
+
+    this.map.resize()
+  }
 }
