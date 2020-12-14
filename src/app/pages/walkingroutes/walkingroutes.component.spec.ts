f8a57121ae2f4cc9a2114eea50ac2730e946d6d4
DESKTOP-BN5JHCD\rhian <rhian.rose@landmark.co.uk>
2020-12-14 20:28:17 +0000

    Updated packages to fix heroku deployment

--- src/app/pages/walkingroutes/walkingroutes.component.spec.ts
+++ src/app/pages/walkingroutes/walkingroutes.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WalkingroutesComponent } from './walkingroutes.component';
 
@@ -6,7 +6,7 @@ describe('WalkingroutesComponent', () => {
   let component: WalkingroutesComponent;
   let fixture: ComponentFixture<WalkingroutesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WalkingroutesComponent ]
     })
