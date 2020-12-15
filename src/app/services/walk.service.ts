d3cffb25564d3bdf74762b3d1cc1d0b21d4938cf
DESKTOP-BN5JHCD\rhian <rhian.rose@landmark.co.uk>
2020-12-15 10:54:47 +0000

    Updated walks api url to deployed api url

--- src/app/services/walk.service.ts
+++ src/app/services/walk.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8080/api/walks';
+const baseUrl = 'https://walkies-app-api.herokuapp.com/api/walks';
 
 @Injectable({
     providedIn: 'root'
