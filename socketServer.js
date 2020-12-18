30327ae3861d09861b807af369c0f23a67ab72ab
DESKTOP-BN5JHCD\rhian <rhian.rose@landmark.co.uk>
2020-12-18 20:18:49 +0000

    ITS WORKING!

--- socketServer.js
+++ socketServer.js
@@ -2,24 +2,36 @@ let express = require("express");
 let path = require("path");
 let http = require("http");
 let socketIo = require("socket.io");
-
+// Set up the app and server.
 let app = express();
 let server = http.createServer(app);
+let cors = require("cors")
 
+// Configure statics.
 app.use(express.static(path.join(__dirname, "resources")));
 
-let io = socketIo(server);
+// Initialise the socket server.
+//let io = socketIo(server);
 
-io.on("connection", function (socket) {
-  console.log("a user connected");
-
-  socket.emit("server message", "Hello World!");
-
-  socket.on("client message", function (msg) {
-    console.log("Red'd from client: '" + msg + "'");
-  });
+let io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["*"],
+    credentials: true
+  }
 });
 
+// "On connection" handler.
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  // Send a message to the client.
+  socket.emit("server message", "Hello World");
+  // Handler for messages from the client.
+  socket.on("client message", function (msg) {
+    console.log("Rec’d from client: ’" + msg + "’");
+  });
+});
 server.listen(9000, () => {
-  console.log("Listening on port 9000")
-})
+  console.log("Listening on 9000");
+});
